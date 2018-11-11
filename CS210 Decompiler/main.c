80b2d9830a6c2eb2b93a88c398ef9539e0d299db
RossWilliamson1 <39889940+RossWilliamson1@users.noreply.github.com>
2018-11-11 19:28:11 +0000

    Add files via upload
    Came up with a few errors when trying to compare struct Word and int

--- CS210 Decompiler/main.c
+++ CS210 Decompiler/main.c
@@ -30,7 +30,7 @@ struct Word main_memory[4096];
 // Returns n'th bit from the input given to it
 int getNthBit(int input, int desired_bit); 
 struct Word* getMainMemory(); //Access as a pointer so we can use it as a massive long 'word' 
-
+void opCodeFunctions(struct Instruction exInstruct, struct CPU comp, int x); // x will be the decimal version of the instruction, passed in to be added to MAR
 
 
 int main() {
@@ -54,4 +54,50 @@ int getNthBit(int input, int desired_bit) {
 */
 struct Word* getMainMemory(int location) {
 	return &main_memory[location];
+}
+
+void opCodeFunctions (struct Instruction exInstruct, struct CPU comp, int x) {
+	for (comp.pc == 0; comp.pc < 1000; comp.pc++) {
+		
+		comp.ir = main_memory[comp.pc];
+		comp.mar = x; 
+		comp.mbr = main_memory[comp.mar];
+		
+		switch(exInstruct.opcode) {
+			case 0: break;
+			
+			case 1: comp.ac = comp.mbr;
+			break;
+			
+			case 2: main_memory[comp.mbr] = comp.ac;
+			break;
+			
+			case 3: comp.ac -= comp.mbr;
+			break;
+			
+			case 4: comp.ac += comp.mbr;
+			break;
+			
+			case 5: 
+				printf("Please enter the value you would like to input into the Accumulator \n");
+				scanf("%d", &comp.ac);
+				comp.input_register = comp.ac;
+			break;
+			
+			case 6:
+				comp.output_register = comp.ac;
+				printf("The value currently stored in the Accumulator is %d", comp.ac);
+			break;
+			
+			case 7: 
+				if (comp.mbr != 1024) {
+					comp.pc+=1;
+				}
+			break;
+			
+			case -1: comp.pc = comp.mbr;
+			
+			
+		}
+	}
 }
